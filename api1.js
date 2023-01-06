b522899c3f891e0b666cd95443a1dc80a4f64a4e
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2023-01-06 20:03:16 +0530

    main

---
+++ api1.js
@@ -0,0 +1,34 @@
+{
+    "meals": [
+    {
+    "id": 640965,
+    "imageType": "jpg",
+    "title": "Crushed Lentil Soup- Granola Style",
+    "readyInMinutes": 45,
+    "servings": 1,
+    "sourceUrl": "https://spoonacular.com/crushed-lentil-soup-granola-style-640965"
+    },
+    {
+    "id": 643674,
+    "imageType": "jpg",
+    "title": "Fried Brown Rice",
+    "readyInMinutes": 25,
+    "servings": 2,
+    "sourceUrl": "https://spoonacular.com/fried-brown-rice-643674"
+    },
+    {
+    "id": 654935,
+    "imageType": "jpg",
+    "title": "Pasta with Peas and Italian Sausage",
+    "readyInMinutes": 45,
+    "servings": 4,
+    "sourceUrl": "https://spoonacular.com/pasta-with-peas-and-italian-sausage-654935"
+    }
+    ],
+    "nutrients": {
+    "calories": 1875.8,
+    "protein": 47.19,
+    "fat": 68.72,
+    "carbohydrates": 273.97
+    }
+    }
